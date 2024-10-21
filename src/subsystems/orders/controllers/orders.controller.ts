beefade7e2f5f5a3a51857190a745a25e0b0b7ea
Ernesto Rodriguez Castillo <developer1575@gmail.com>
2024-10-21 15:50:02 -0400

    creating order controller, and orders DTO

---
+++ src/subsystems/orders/controllers/orders.controller.ts
@@ -0,0 +1,80 @@
+import {
+    UseGuards,
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Query,
+    Delete,
+  } from '@nestjs/common';
+  import {
+    ApiTags,
+    ApiBearerAuth,
+    ApiUnauthorizedResponse,
+    ApiCreatedResponse,
+    ApiForbiddenResponse
+  } from '@nestjs/swagger';
+import { ProductEntity } from 'src/subsystems/products/entity/product.entity';
+
+import { roles } from 'src/subsystems/roles/enum/roles.enum';
+import { Roles } from 'src/subsystems/roles/decorators/roles.decorator';
+import { LocalAuthGuard } from 'src/subsystems/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/subsystems/auth/guards/roles.guard';
+import { OrderService } from '../services/orders.service';
+import { CreateOrderDTO } from '../dto/CreateOrderDTO';
+import { OrderEntity } from '../entities/order.entity';
+
+
+  
+  
+  @ApiTags('cart')
+  @ApiBearerAuth()
+  @Controller('cart')
+  @UseGuards(LocalAuthGuard)
+  @UseGuards(RolesGuard)
+  
+  export class OrderControllers {
+    constructor(private readonly productservice: OrderService) { }
+  
+    @ApiCreatedResponse({ description: 'The record has been created successfully created' })
+    @ApiForbiddenResponse({ description: 'Forbidden' })
+    
+    @Post()
+    @Roles(roles.Admin)
+    create(@Body() createUserDto: CreateOrderDTO) {
+      return this.productservice.create(createUserDto);
+    }
+  
+
+    
+    //@UseGuards(JwtAuthGuard)
+    @Get()
+    @Roles(roles.Admin)
+    public getUsers(): Promise<OrderEntity[]> { 
+      return this.productservice.findAll();
+    }
+  
+  
+    @Get(':id')
+    @Roles(roles.Admin)
+    getUserById(@Param('id') id: string) {
+      return this.productservice.findOneById(+id);
+    }
+  
+  
+    @Patch(':id')
+    @Roles(roles.Admin)
+    updateUser(@Param('id') id: string, @Body() updateUserDto) {
+      return this.productservice.update(+id, updateUserDto);
+    }
+  
+  
+    @Delete(':id')
+    @Roles(roles.Admin)
+    deleteUser(@Param('id') id: string) {
+      //return this.userService.deleteUser(+id);
+      return this.productservice.softDelete(+id);
+    }
+  }
---
+++ src/subsystems/orders/dto/CreateOrderDTO.ts
@@ -0,0 +1,42 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { BaseDto } from 'src/common/dto/base.dto';
+import { CartEntity } from 'src/subsystems/cart/entity/cart.entity';
+import { User } from 'src/subsystems/user/entities/user.entity';
+
+export class CreateOrderDTO extends BaseDto {
+  @ApiProperty({
+        example: "User structure",
+        description: "A valid quantity",
+    })
+    @IsNotEmpty()
+    user:User
+    @ApiProperty({
+        example: "+5358126024",
+        description: "a phone number",
+    })
+    @IsNotEmpty()
+    @IsString()
+    phone :string
+    @ApiProperty({
+        example: "Finca tirabeque edificio 2 apartamento 5 ",
+        description: "an adress",
+    })
+    @IsNotEmpty()
+    @IsString()
+    address :string
+
+    @ApiProperty({
+        example: "031229681709",
+        description: "a valid cuban id",
+    })
+    @IsNotEmpty()
+    @IsNumber()
+    CI :number
+    @ApiProperty({
+        example: "Cart structure",
+        description: "a valid cuban id",
+    })
+    @IsNotEmpty()
+    carts: CartEntity[];
+}
