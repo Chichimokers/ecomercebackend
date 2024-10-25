33d030b60d286a223e5442acdf9acf40734adf5c
Ernesto Rodriguez Castillo <developer1575@gmail.com>
2024-10-25 12:34:35 -0400

    Controller complleted

--- src/subsystems/payments/controller/paypal.controller.ts
+++ src/subsystems/payments/controller/paypal.controller.ts
@@ -5,8 +5,7 @@ import { LocalAuthGuard } from "src/subsystems/auth/guards/jwt-auth.guard";
 import { RolesGuard } from "src/subsystems/auth/guards/roles.guard";
 import { Roles } from "src/subsystems/roles/decorators/roles.decorator";
 import { roles } from "src/subsystems/roles/enum/roles.enum";
-
-
+import {HOST} from "../config.payments"
 // Controller
 @ApiTags('payments')
 @ApiBearerAuth()
@@ -20,7 +19,29 @@ export class PaypalController {
     @Roles(roles.User)
     @Post("create-order")
     async createOrder(){
-        
+
+        const order = {
+            intent:"CAPTURE",
+            purchase_unit:[
+
+                {
+                    amount: {
+                        concurrency_code:"USD",
+                        value:"100.0"
+                    }
+                  
+                }
+            ],
+            application_context:{
+                brand_name:"ESAKISHOP",
+                landing_page:"NO_PREFERENCE",
+                user_action:"pay_now",
+                return_url:`${HOST}/return`
+
+            }
+
+        }
+
     }
 
         
