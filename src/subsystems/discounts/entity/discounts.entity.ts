6d8062b973288a4272fe0f18805d18b6abdb2016
JleivSuaxy <jleivsuaxy@gmail.com>
2024-12-02 21:24:13 -0500

    Security cape for discount

--- src/subsystems/discounts/entity/discounts.entity.ts
+++ src/subsystems/discounts/entity/discounts.entity.ts
@@ -1,13 +1,26 @@
-import { Column, Entity, OneToOne } from "typeorm";
+import { Check, Column, Entity, OneToOne } from "typeorm";
 import { BaseEntity } from "../../../common/entities/base.entity";
 import { ProductEntity } from "../../products/entity/product.entity";
+import { IsNumber, IsPositive } from "class-validator";
 
 @Entity({ name: "tb_discounts" })
+@Check(`"min" > 0`)
+@Check(`"reduction" > 0`)
 export class DiscountEntity extends BaseEntity {
+    /**
+     * Minimum to validate that the purchase is discounted.
+     */
     @Column()
+    @IsNumber()
+    @IsPositive()
     min: number;
 
+    /**
+     * Price reduction per unit, once the discount is applied
+     */
     @Column()
+    @IsNumber()
+    @IsPositive()
     reduction: number;
 
     @OneToOne(() => ProductEntity, (product) => product.discounts)
