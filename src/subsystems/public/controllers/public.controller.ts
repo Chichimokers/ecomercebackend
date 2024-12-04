19d4370601734e7a7de7ca63884c12635f824f33
JleivSuaxy <jleivsuaxy@gmail.com>
2024-12-04 14:33:36 -0500

    building public

--- src/subsystems/public/controllers/public.controller.ts
+++ src/subsystems/public/controllers/public.controller.ts
@@ -1,23 +1,23 @@
-import { LocalAuthGuard } from "../../auth/guards/jwt-auth.guard";
-import { RolesGuard } from "../../auth/guards/roles.guard";
-import { Body, Controller, Post, Req, UseGuards } from "@nestjs/common";
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
-import { BuildOrderDTO } from "../dto/frontsDTO/ordersDTO/buildorder.dto";
-import { OrderService } from "../../orders/services/orders.service";
+import { LocalAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../../auth/guards/roles.guard';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { BuildOrderDTO } from '../dto/frontsDTO/ordersDTO/buildorder.dto';
+import { OrderService } from '../../orders/services/orders.service';
+import { Roles } from '../../roles/decorators/roles.decorator';
+import { roles } from '../../roles/enum/roles.enum';
 
 @ApiTags('public')
 @ApiBearerAuth()
 @Controller('public')
 @UseGuards(LocalAuthGuard, RolesGuard)
-export class PublicController{
-    constructor(private orderService: OrderService) {
-    }
+export class PublicController {
+    constructor(private orderService: OrderService) {}
 
     @Post()
-    createOrder(@Req() request: any, @Body() orderdto: BuildOrderDTO){
+    @Roles(roles.User)
+    createOrder(@Req() request: any, @Body() orderdto: BuildOrderDTO) {
         const userid = request.user.Id;
-        console.log(userid);
-        console.log(orderdto);
         return this.orderService.createOrderService(userid, orderdto);
     }
 }
