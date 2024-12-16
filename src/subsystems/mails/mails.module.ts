1a7a38a40b0412422c56b1c7f039a712bbc3251a
Ernesto Rodriguez Castillo <developer1575@gmail.com>
2024-12-16 13:27:52 -0500

    ADD:fly.io deploy

--- src/subsystems/mails/mails.module.ts
+++ src/subsystems/mails/mails.module.ts
@@ -1,9 +1,23 @@
 import { Module } from "@nestjs/common";
 import { MailsService } from "./services/mails.service";
+import { MailerModule } from "@nestjs-modules/mailer";
 
 @Module({
-    imports:[],
-    providers:[MailsService],
-    controllers:[],
+    imports: [
+        MailerModule.forRoot({
+          transport: {
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // true para SSL/TLS
+            auth: {
+              user: 'tu-correo@gmail.com', // Tu correo
+              pass: 'contraseña-de-aplicación', // Contraseña de aplicación
+            },
+          },
+          defaults: {
+            from: '"Tu Nombre" <tu-correo@gmail.com>', // Remitente por defecto
+          },
+        }),
+      ],
 })
 export class MailsModule {}
