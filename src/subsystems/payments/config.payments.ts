ef3d52878e5c133084564caf64ce13cb042a94d6
developer1575@gmail.com <developer1575@gmail.com>
2025-01-20 00:20:49 -0500

    ADD: enviroment

--- src/subsystems/payments/config.payments.ts
+++ src/subsystems/payments/config.payments.ts
@@ -1,7 +1,8 @@
-export const CLIENTID = "Ab0ReLbfKPCOJ1KhwRkm13JI-icCoYYo7yUGrFQpQ5wUUQBY2oLBaX3X4KDRLy6KGkwN-Nr2Vj2RNapQ";
-export const SECRET_KEY="ECDLd2LHthO83E_PiCwgFXzOXgIDckYjU15n0KJe4K2ZAFnI0fc_ABL47kLuRBilWjaqE3Q_C47PGMjB";
+export const CLIENTID = process.env.CLIENTID || "Ab0ReLbfKPCOJ1KhwRkm13JI-icCoYYo7yUGrFQpQ5wUUQBY2oLBaX3X4KDRLy6KGkwN-Nr2Vj2RNapQ";
+export const SECRET_KEY= process.env.SECRET_KEY || "ECDLd2LHthO83E_PiCwgFXzOXgIDckYjU15n0KJe4K2ZAFnI0fc_ABL47kLuRBilWjaqE3Q_C47PGMjB";
 
-export const HOST = process.env.HOST_WEB || "http://localhost:3000"
+export const HOST = process.env.HOST || "http://localhost:3000"
 
-export const PAYPAL_HOST = "https://api-m.sandbox.paypal.com"
-export const STRIPE_SECRET_KEY = "sk_test_51QDsbwGCEqKXLQNB8H2safwcgJteiPhS0IdEEMHJsjIFhabfg4p4P0TX7Qv08BwdLl9eWSDmBb97A0cMEQ5mvTLE003RdOzNAw"
+export const PAYPAL_HOST = process.env.PAYPAL_HOST || "https://api-m.sandbox.paypal.com"
+
+export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || "sk_test_51QDsbwGCEqKXLQNB8H2safwcgJteiPhS0IdEEMHJsjIFhabfg4p4P0TX7Qv08BwdLl9eWSDmBb97A0cMEQ5mvTLE003RdOzNAw"
