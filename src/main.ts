2ad257bb1987d165ee77435df56723fae5611181
Ernesto Rodriguez Castillo <developer1575@gmail.com>
2025-01-22 00:51:49 -0500

    a

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
 
     const app = await NestFactory.create(AppModule,{httpsOptions});
 
-    
+
 
     app.enableCors({
         origin: 'http://localhost:3000',
@@ -25,9 +25,9 @@ async function bootstrap() {
 
     DocapiBuilder(app);
 
-    await app.listen(process.env.APP_PORT||  443);
+    await app.listen(process.env.APP_PORT||  8080);
 
     //console.log("Escuchando en puerto 8000");
 
 }
-bootstrap().then(() => console.log("Server running on port " + process.env.APP_PORT||  443));
+bootstrap().then(() => console.log("Server running on port " + process.env.APP_PORT||  8080));
