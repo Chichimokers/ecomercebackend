800604e845625098e31757834361ddd8f683d6a4
Ernesto Rodriguez Castillo <developer1575@gmail.com>
2025-02-02 16:01:31 -0500

    lll

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
 
     app.enableCors({
-        origin: 'http://localhost:3000',
+        origin:['https://esaki-jrr.com',"http://localhost:3000"],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
     });
