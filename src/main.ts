02a6a3bb7b297d4d1159a288b5760b2295c1b81b
jleivsuaxy <jleivsuaxy@gmail.com>
2025-02-07 17:36:37 -0500

    Modified environments in branch
    Signed-off-by: jleivsuaxy <jleivsuaxy@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocapiBuilder } from './swagger';
-import { appendFile } from 'fs';
-import * as fs from 'fs';
-import * as https from 'https';
 
 async function bootstrap() {
 
@@ -25,4 +22,4 @@ async function bootstrap() {
     //console.log("Escuchando en puerto 8000");
 
 }
-bootstrap().then(() => console.log("Server running on port " + process.env.APP_PORT||  8080));
+bootstrap().then(() => console.log("Server running on port " + process.env.APP_PORT || 8080));
