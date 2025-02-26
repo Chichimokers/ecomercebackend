be10230e68b787545bd86c9837386216459f9f32
AGR55 <gonzalezreyesadriano@gmail.com>
2025-02-26 00:55:07 -0500

    xd

--- src/subsystems/payments/controller/paypal.controller.ts
+++ src/subsystems/payments/controller/paypal.controller.ts
@@ -1,12 +1,24 @@
 // Import Line
-import { Req, UseGuards, Controller, Get, Post, Query, Inject, HttpCode, HttpStatus, Res, Body } from "@nestjs/common";
-import { ApiTags, ApiBearerAuth } from "@nestjs/swagger";
-import { LocalAuthGuard } from "src/subsystems/auth/guards/jwt-auth.guard";
-import { RolesGuard } from "src/subsystems/auth/guards/roles.guard";
-import { Roles } from "src/subsystems/roles/decorators/roles.decorator";
-import { roles } from "src/subsystems/roles/enum/roles.enum";
+import {
+    Req,
+    UseGuards,
+    Controller,
+    Get,
+    Post,
+    Query,
+    Inject,
+    HttpCode,
+    HttpStatus,
+    Res,
+    Body,
+} from '@nestjs/common';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { LocalAuthGuard } from 'src/subsystems/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/subsystems/auth/guards/roles.guard';
+import { Roles } from 'src/subsystems/roles/decorators/roles.decorator';
+import { roles } from 'src/subsystems/roles/enum/roles.enum';
 import { PaypalService } from '../service/paypal.service';
-import { Response } from "express";
+import { Response } from 'express';
 
 // Controller
 @ApiTags('payments')
@@ -15,31 +27,40 @@ import { Response } from "express";
 export class PaypalController {
     constructor(
         @Inject(PaypalService)
-        public servicePaypal:PaypalService
+        public servicePaypal: PaypalService,
     ) { }
-    
-    @UseGuards(LocalAuthGuard,RolesGuard)
-    @Roles(roles.User)
-    @Post("create-order")
-    async createOrder(@Body() body:any ,@Res() res: Response,@Req() req:any ): Promise<void> {
-         
-        const link: string = await this.servicePaypal.CreateOrder(body.id,req.user.Id);
 
-        res.send(link)
+    @UseGuards(LocalAuthGuard, RolesGuard)
+    @Roles(roles.User)
+    @Post('create-order')
+    async createOrder(
+        @Body() body: any,
+        @Res() res: Response,
+        @Req() req: any,
+    ): Promise<void> {
+        const link: string = await this.servicePaypal.CreateOrder(
+            body.id,
+            req.user.Id,
+        );
+
+        res.send(link);
     }
 
-    @Get("capture-order")
+    @Get('capture-order')
     @HttpCode(HttpStatus.OK)
-    async captureOrder(@Query('token') token: string, @Query('PayerID') payerId: string) {
+    async captureOrder(
+        @Query('token') token: string,
+        @Query('PayerID') payerId: string,
+    ) {
         // Usa el token y payerId según sea necesario
         const response: boolean = await this.servicePaypal.confirmorder(token);
 
-        return response ? { success: true } : { success: false, errorCode: 400 };
+        return response
+            ? { success: true }
+            : { success: false, errorCode: 400 };
     }
 
     @Roles(roles.User)
-    @Post("cancel-order")
-    async cancelorder(): Promise<void> {
-        
-    }
+    @Post('cancel-order')
+    async cancelorder(): Promise<void> { }
 }
