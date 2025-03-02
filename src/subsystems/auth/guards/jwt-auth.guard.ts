78d642cde628a84ab73b5b9f50ebbe4c122f94c0
Ernesto Rodriguez Castillo <developer1575@gmail.com>
2025-03-02 18:11:35 -0500

    ggggg

--- src/subsystems/auth/guards/jwt-auth.guard.ts
+++ src/subsystems/auth/guards/jwt-auth.guard.ts
@@ -4,12 +4,5 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('jwt') {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    console.log("Entrando a JWT AuthGuard");
-    
-    // Ejecuta la lógica original del AuthGuard('jwt')
-    return super.canActivate(context);
-  }
+
 }
