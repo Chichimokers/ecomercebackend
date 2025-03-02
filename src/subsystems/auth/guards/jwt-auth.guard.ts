b56eda3c3a16242fe692b2b1bbd3b1aba3423d70
Ernesto Rodriguez Castillo <developer1575@gmail.com>
2025-03-02 17:58:45 -0500

    dddd

--- src/subsystems/auth/guards/jwt-auth.guard.ts
+++ src/subsystems/auth/guards/jwt-auth.guard.ts
@@ -3,11 +3,13 @@ import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class LocalAuthGuard  extends AuthGuard('jwt'){
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-     console.log("entrando a jwt auth guard")
-     return true;
-    }
-
-   
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    console.log("Entrando a JWT AuthGuard");
+    
+    // Ejecuta la lógica original del AuthGuard('jwt')
+    return super.canActivate(context);
+  }
 }
