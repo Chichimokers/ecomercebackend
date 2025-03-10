e876b70e05e2b338d86c67322f4a23d87230b935
jleivsuaxy <jleivsuaxy@gmail.com>
2025-03-09 21:24:35 -0400

    Fixed errors in merge.

--- src/subsystems/public/controllers/userpublic.controller.ts
+++ src/subsystems/public/controllers/userpublic.controller.ts
@@ -1,54 +1,32 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { BuildOrderDTO } from '../dto/frontsDTO/ordersDTO/buildorder.dto';
-import { OrderService } from '../../orders/services/orders.service';
-import { Roles } from '../../roles/decorators/roles.decorator';
-import { roles } from '../../roles/enum/roles.enum';
-import { LocalAuthGuard } from '../../auth/guards/jwt-auth.guard';
-import { RolesGuard } from '../../auth/guards/roles.guard';
-import { DeleteOrderDTO } from '../dto/frontsDTO/ordersDTO/deleteorder.dto';
-import { PaypalService } from 'src/subsystems/payments/service/paypal.service';
-import { OrderEntity } from 'src/subsystems/orders/entities/order.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { CalcEnvioDTO } from '../dto/frontsDTO/ordersDTO/calcenvio.dto';
+import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { BuildOrderDTO } from "../dto/frontsDTO/ordersDTO/buildorder.dto";
+import { OrderService } from "../../orders/services/orders.service";
+import { Roles } from "../../roles/decorators/roles.decorator";
+import { roles } from "../../roles/enum/roles.enum";
+import { LocalAuthGuard } from "../../auth/guards/jwt-auth.guard";
+import { RolesGuard } from "../../auth/guards/roles.guard";
+import { DeleteOrderDTO } from "../dto/frontsDTO/ordersDTO/deleteorder.dto";
+import { PaypalService } from "src/subsystems/payments/service/paypal.service";
 
-@ApiTags('userpublic')
+@ApiTags("userpublic")
 @ApiBearerAuth()
-@Controller('userpublic')
+@Controller("userpublic")
 @UseGuards(LocalAuthGuard, RolesGuard)
 export class UserPublicController {
-    constructor(private orderService: OrderService,private paypalservice :PaypalService,
-     @InjectRepository(OrderEntity)
-    private orderRepository : Repository<OrderEntity>) { }
+    constructor(
+        private orderService: OrderService, private paypalservice: PaypalService
+    ) {
+    }
 
-    @Post('/create-order')
+    @Post("/create-order")
     @Roles(roles.User)
     createOrder(@Req() request: any, @Body() orderdto: BuildOrderDTO) {
         const userid = request.user.Id;
         return this.orderService.createOrderService(userid, orderdto);
-    }  
+    }
 
-    /*@Post('/calcular_envio')
-    @Roles(roles.User)
-    async  calcenvio(@Req() request: any,@Body() boduy : CalcEnvioDTO) {
-        
-        const userid = request.user.Id;
-        const orderbd: OrderEntity = await this.orderRepository.findOne({
-            where: { id: boduy.orderId },
-            relations: [
-                'orderItems',
-                'orderItem.product',
-                'orderItem.product.discounts',
-                'municipality', // Relación directa de OrderEntity -> MunicipalityEntity
-                'municipality.prices', // Relación MunicipalityEntity -> PriceByWeightEntity
-            ],
-        });
-
-        return this.paypalservice.calcularprecio_envio(orderbd)
-    }*/
-
-    @Post('/retire-order')
+    @Post("/retire-order")
     @Roles(roles.User)
     retireOrder(@Req() request: any, @Body() orderdto: DeleteOrderDTO) {
         const userid = request.user.Id;
@@ -56,7 +34,7 @@ export class UserPublicController {
     }
 
     // *--- For Order View ---* //
-    @Get('/orders')
+    @Get("/orders")
     @Roles(roles.User)
     public getUserOrder(@Req() request: any) {
         const userid = request.user.Id;
